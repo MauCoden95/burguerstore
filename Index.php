a21e768dbc880e712badaec6291ef29243be0d80
Mauro Miguel <32025338+MauCoden95@users.noreply.github.com>
2023-01-26 00:01:39 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Assets/Styles.css">
+    <link rel="shortcut icon" href="./Assets/Img/Logo.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/7483adbd94.js" crossorigin="anonymous"></script>
+    <title>BurguerStore</title>
+</head>
+<body>
+    <section id="div-contact-header">
+        <div class="center">
+            <div class="div-contact-header-1">
+                <h3><i class="fas fa-envelope"></i> burguershop@email.com</h3>
+                <h3><i class="fas fa-phone"></i> (54 11) 4999-1200</h3>
+            </div>
+            <div class="div-contact-header-2">
+                <a href="" class="fab fa-facebook"></a>
+                <a href="" class="fab fa-instagram"></a>
+                <a href="" class="fab fa-twitter"></a>
+                <a href="" class="fab fa-linkedin"></a>
+                <a href="" class="div-contact-header-2-a">Login</a>
+            </div>
+        </div>
+        
+    </section>
+
+
+    <header id="header">
+        <div class="center">
+            <img src="./Assets/Img/Logo.png" alt="Logo">
+
+            <nav id="navbar">
+                <ul>
+                    <li><a href="">Inicio</a></li>
+                    <li><a href="">Nosotros</a></li>
+                    <li><a href="">Menu</a></li>
+                    <li><a href="">Reseñas</a></li>
+                    <li><a href="">Contacto</a></li>
+                </ul>
+            </nav>
+
+            <div class="header-info">
+                <h3>Días abierto:</h3>
+                <p>Lunes a viernes 10 a 00</p>
+            </div>
+        </div>
+    </header>
+
+
+    <section id="banner">
+        <div class="center">
+            <h1>Probá nuestra <span>Fantasy Burguer</span></h1>
+            <img src="./Assets/Img/Banner.png" alt="Hamburguesa">
+        </div>
+    </section>
+
+
+    <section id="about">
+        <div class="center">
+            <h2 class="section-title">-Quienes somos-</h2>
+        </div>
+    </section>
+</body>
+</html>
